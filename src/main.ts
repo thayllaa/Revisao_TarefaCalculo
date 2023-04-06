2aa74c2d9a87b19637066341da3c8fb3e31dad81
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-05 21:55:03 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { PriceComponent } from './app/components/price/price.component';
 import { AmortizacaoComponent } from './app/components/amortizacao/amortizacao.component';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [ AppModule, CommonModule, PriceComponent, AmortizacaoComponent],
+  imports: [CommonModule, PriceComponent, AmortizacaoComponent],
+  template: ``,
 })
+export class App {
+}
 
-export class AppModule {}
+bootstrapApplication(App);
