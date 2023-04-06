719e19b3297f0957d774814198d9d8a4bc2728ad
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-05 21:29:49 -0300

    8 files added

---
+++ src/app/components/amortizacao/amortizacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amortizacao',
+  templateUrl: './amortizacao.component.html',
+  styleUrls: ['./amortizacao.component.css']
+})
+export class AmortizacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
