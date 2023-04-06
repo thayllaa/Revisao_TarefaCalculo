b245e4a384be1cf2ded7308b6b263bf02b624f14
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-05 21:43:08 -0300

    1 file modified

--- src/app/components/price/price.component.ts
+++ src/app/components/price/price.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-price',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./price.component.css']
 })
 export class PriceComponent implements OnInit {
+  @Input() PV: string;
+  @Input() i: string;
+  @Input() n: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getPrice() {
+    return Number(this.PV) * (((Math.pow(1 + Number(this.i), Number(this.n))) * Number(this.i) / ((Math.pow(1 + Number(this.i), Number(this.n)) -1))));
+  }
 }
