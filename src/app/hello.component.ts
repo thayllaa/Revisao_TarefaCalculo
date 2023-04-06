f68f88905d2665a92ff03d5181e4c8f1cb3418c0
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-05 21:30:41 -0300

    2 files modified

---
+++ src/app/hello.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `<h1> {{name}} </h1>`,
+  styles: [`h1 {font-family: Times New Roman}`]
+})
+
+export class HelloComponent {
+  @Input() name: string;
+}
